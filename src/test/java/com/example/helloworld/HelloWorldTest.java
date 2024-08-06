e7d6c10e86cbfd63af2880087bf761eeff57f97d
pc9733 <piyushchoudhry9733@gmail.com>
2024-08-06 21:43:23 +0530

    first commit

---
+++ src/test/java/com/example/helloworld/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testMain() {
+        // This is a simple test case
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
+
